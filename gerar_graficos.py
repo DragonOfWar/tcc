7aa2c491dddcfb5ba177008ee5bd85fdb728fcdd
DragonOfWar <therealdragonofwar@gmail.com>
2022-04-27 08:18:27 -0300

    Adicionar teste de friedman-nemenyi

--- gerar_graficos.py
+++ gerar_graficos.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import scikit_posthocs as sp
+import numpy as np
 import csv
 import os
+from scipy import stats
 
 # Config
 QNT_ITERACOES = 5
@@ -14,6 +17,7 @@ def criar_dir(dir):
 
 
 CAMINHO_RESULTADOS_RAW = "resultados_raw"
+CAMINHO_RESULTADOS = "resultados"
 CAMINHO_FIGURAS_ACURACIAS = "graficos_acuracias"
 CAMINHO_FIGURAS_KAPPA = "graficos_kappa"
 criar_dir(CAMINHO_FIGURAS_ACURACIAS)
@@ -21,10 +25,10 @@ criar_dir(CAMINHO_FIGURAS_KAPPA)
 
 
 # Gerar graficos das acuracias
-def gerar_graficos(coluna, dir):
+def gerar_graficos(coluna, dir, nome):
     for d in DATASETS:
         for i in range(1, QNT_ITERACOES):
-            print(f"Processando {d} iteração {i}", end="\r")
+            print(f"Gerando gráfico de {nome}: processando {d} iteração {i}", end="\r")
             ys = {c: [] for c in CLASSIFICADORES}
             xs = []
             xs_pronto = False
@@ -45,13 +49,50 @@ def gerar_graficos(coluna, dir):
                 xs_pronto = True
             for c, y in ys.items():
                 plt.plot(xs, y, label=c)
-            plt.xlabel("acuracia")
-            plt.ylabel("amostras")
+            plt.xlabel("amostras")
+            plt.ylabel(nome)
             plt.legend()
             plt.grid()
             plt.savefig(f"{dir}/{d}_{i}.png")
             plt.clf()
 
 
-gerar_graficos(2, CAMINHO_FIGURAS_ACURACIAS)
-gerar_graficos(4, CAMINHO_FIGURAS_KAPPA)
+gerar_graficos(2, CAMINHO_FIGURAS_ACURACIAS, "acuracia")
+gerar_graficos(4, CAMINHO_FIGURAS_KAPPA, "kappa")
+
+# Executar teste de friedman-nemenyi nas acuracias
+acuracias = {c: [] for c in CLASSIFICADORES}
+kappas = {c: [] for c in CLASSIFICADORES}
+tempos = {c: [] for c in CLASSIFICADORES}
+
+COLUNA_ACURACIA = 1
+COLUNA_KAPPA = 2
+COLUNA_TEMPO = 5
+
+# Ler resultados
+for d in DATASETS:
+    for c in CLASSIFICADORES:
+        with open(f"{CAMINHO_RESULTADOS}/resultados_{c}_{d}.csv") as csvfile:
+            csvr = csv.reader(csvfile, delimiter=",")
+            next(csvr)  # Descartar primeira linha (cabeçalho)
+            for l in csvr:
+                acuracias[c].append(l[COLUNA_ACURACIA])
+                kappas[c].append(l[COLUNA_KAPPA])
+                tempos[c].append(l[COLUNA_TEMPO])
+
+# Teste
+fsr_a = stats.friedmanchisquare(*list(acuracias.values()))
+fsr_k = stats.friedmanchisquare(*list(kappas.values()))
+fsr_t = stats.friedmanchisquare(*list(tempos.values()))
+
+print("FSR Acuracias: ", fsr_a)
+print("FSR Kappa: ", fsr_k)
+print("FSR Tempos: ", fsr_t)
+
+data_a = np.array(list(acuracias.values()))
+data_k = np.array(list(kappas.values()))
+data_t = np.array(list(tempos.values()))
+
+print(sp.posthoc_nemenyi_friedman(data_a.T))
+print(sp.posthoc_nemenyi_friedman(data_k.T))
+print(sp.posthoc_nemenyi_friedman(data_t.T))
